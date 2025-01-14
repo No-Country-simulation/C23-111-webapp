dd61a0e231c75b713c58e758298152819c3f687e
Antonio Navarro <anavarro67@hotmail.com>
2025-01-14 19:53:29 +0100

    feat: Se agrega la función controladora para borrar una reseña

--- server/src/modules/rates/rates.routes.ts
+++ server/src/modules/rates/rates.routes.ts
@@ -2,12 +2,12 @@ import express from "express";
 import ratesController from "./rates.controller"
 import { validateSchema } from "../../middleware/validateSchema";
 import { rateSchema } from "./rates.schema";
-const { createRate } = ratesController;
+const { createRate, deleteRate } = ratesController;
 
 const ratesRoutes = express.Router();
 
 ratesRoutes.post("/", validateSchema(rateSchema), createRate);
-
+ratesRoutes.delete("/:id", deleteRate);
 
 export default ratesRoutes;
 
