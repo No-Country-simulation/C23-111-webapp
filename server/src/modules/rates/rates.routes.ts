c36de27b37bcd6d08057f97f4725aeb1e74c5927
Antonio Navarro <anavarro67@hotmail.com>
2025-01-14 19:40:20 +0100

    feat: Se agrega el campo recipe al modelo rate; Se agregan las validaciones de los campos con Zod

--- server/src/modules/rates/rates.routes.ts
+++ server/src/modules/rates/rates.routes.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import ratesController from "./rates.controller"
-
+import { validateSchema } from "../../middleware/validateSchema";
+import { rateSchema } from "./rates.schema";
 const { createRate } = ratesController;
 
 const ratesRoutes = express.Router();
 
+ratesRoutes.post("/", validateSchema(rateSchema), createRate);
 
-ratesRoutes.post("/new-rate", createRate);
 
 export default ratesRoutes;
+
