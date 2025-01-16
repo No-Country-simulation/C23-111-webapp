75d24c188eb13294e9f3f746be9a90414664c713
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2025-01-15 21:03:05 -0300

    Refactor: Configuración del servidor
    1. Se instaló un logger para visualizar las solicitudes HTTP en la consola.
       1.a. Se eliminó el console.log de la configuración de Cloudinary.
    
    2. Se creó un archivo de configuración para CORS y se aplicó en el archivo `server.ts`.
    
    3. Cambios en `server.ts`:
       3.a. Se deshabilitó "x-powered-by" para prevenir ciertos ciberataques.
       3.b. Se agregó una ruta de entrada en "/".
       3.c. Se ajustó el formato del mensaje de `server.listen` para permitir un clic directo al navegador.
       3.d. Se eliminó la importación duplicada de `dotenv.config` y se agregó la importación de `HOST`.

---
+++ server/src/config/cors.config.ts
@@ -0,0 +1,15 @@
+import { CORS_ORIGIN } from "./dotenv.config";
+const optionCors = {
+  origin: CORS_ORIGIN,
+  methods: "GET,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  credentials: true,
+  allowedHeaders: [
+    "Access-Control-Allow-Origin",
+    "Content-Type",
+    "Authorization",
+  ],
+};
+
+export { optionCors };
