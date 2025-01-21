6d8fa7e994864605c6ba85b1c25052f5ad72a62d
anavarro81 <87661508+anavarro81@users.noreply.github.com>
2025-01-21 15:33:51 +0100

    Carga de datos incial + Incluir endpoints de reseñas (consulta por id de receta y borrado por id de receta).  (#27)
    * Back juan (#1)
    
    * init commit
    
    * init: inicializacion de backend
    
    * feat: Se crea la conexión a la base de datos de MongoDB.
    
    * fix: Se elimina el fichero package-lock.json
    
    * fix: Se convierte los datos Array para que no de error enla validación. Se corrige error en el nombre del campo ingredients
    
    * feat: Se agrega el campo recipe al modelo rate; Se agregan las validaciones de los campos con Zod
    
    * feat: Se agrega la función controladora para borrar una reseña
    
    * feat: Se elimina la validación de comentario obligatorio. Se crea mvc de la colección de categories.
    
    * fix: eliminar dependencia innecesaria en el schema categories
    
    * feat: Se crea update para actualizar la categoria
    
    * feat: Se crea un endpoint para carga masiva de categorias.
    
    * se suben endpoints de reseñas para consultar por receta y para borrar por id de receta
    
    * Update auth.controller.ts
    
    * Update auth.schema.ts
    
    ---------
    
    Co-authored-by: Juan Ramirez <124215732+juanRCoder@users.noreply.github.com>
    Co-authored-by: Bruno Diaz <135564305+BrunoDiaz7@users.noreply.github.com>

--- server/src/modules/rates/rates.controller.ts
+++ server/src/modules/rates/rates.controller.ts
@@ -1,9 +1,55 @@
 import { Request, Response } from "express";
 import RateModel from "./rates.model";
+import RecipeModel from "../recipes/recipes.model";
+
 
 const createRate = async (req: Request, res: Response): Promise<void> => {
   try {
+    
+    console.log('>> createRate ')
+
+    
     const newRate = new RateModel(req.body);
+
+    console.log('>> newRate ', newRate)
+    
+    // Busco la receta a la que se le quiere agregar la reseña
+    const recipe = await RecipeModel.findById(newRate.recipe);
+
+    console.log('>> recipe ', recipe)
+
+    if (!recipe) {
+      res.status(404).json({ message: "Receta no encontrada" });
+      return;
+    }
+    
+    // Busco todas las reseñas de la receta
+    const ratings = await RateModel.find({ recipe: newRate.recipe })
+    
+    // Agrego la nueva reseña a la lista
+    const totalRates = recipe.totalRates + 1;
+    
+    const totalRatins = ratings.reduce((acc, rate) => acc + rate.rating, 0)
+
+    console.log('>> totalRates', totalRates);
+
+    const rateAverage = (totalRatins + newRate.rating) / totalRates ;
+
+
+
+    console.log('>> ratings', ratings);
+    console.log('>> rateAverage', rateAverage);
+
+    const updatedRecipe = await RecipeModel.findByIdAndUpdate(newRate.recipe, { totalRates, rateAverage }, { new: true })
+
+    console.log('>> updatedRecipe', updatedRecipe);
+
+    if (!updatedRecipe) {
+      res.status(404).json({ message: "Error al actualizar la receta" });
+      return;
+    }
+        
+
     const createdRate = await newRate.save();
     res.status(201).json(createdRate);
   } catch (error) {    
@@ -32,4 +78,63 @@ const deleteRate = async (req: Request, res: Response): Promise<void> => {
   }
 }
 
-export default {createRate, deleteRate};
+const getRatesByRecipe = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { id } = req.params
+
+    const recipeId = id;
+
+    if (!id) {
+      res.status(400).json({ message: "El ID es requerido." });
+      return
+    }
+
+    const result = await RateModel.find({ recipe: recipeId }).populate('reviewer');    
+
+    if (!result) {
+      res.status(404).json({ message: `No hay reseñas para la receta con id ${id}.` });
+      return
+    }
+
+    res.status(200).json({
+      message: "Receta obtenida exitosamente",
+      result,
+    });
+  } catch (error) {
+    
+    res.status(500).json(error);
+    return
+  }
+}
+
+const deleteRatesByRecipeID = async (req: Request, res: Response): Promise<void> => {
+  
+  const { id } = req.params;
+
+  if (!id) {
+    res.status(400).json({ message: "El ID es requerido." });
+    return
+  }
+
+  try {    
+    const deletedRates = await RateModel.deleteMany({ recipe: id });
+    
+    if (!deletedRates) {
+      res.status(404).json({ message: `Reseñas no encontradas para la receta con id = ${id}` });
+      return
+    }
+
+    res.status(200).json({
+      message: "Reseñas eliminadas exitosamente",
+      deletedRates,
+    });
+
+
+  } catch (error) { 
+    res.status(500).json(error);
+  }
+
+}
+
+
+export default {createRate, deleteRate, getRatesByRecipe, deleteRatesByRecipeID};
--- server/src/modules/rates/rates.routes.ts
+++ server/src/modules/rates/rates.routes.ts
@@ -2,12 +2,14 @@ import express from "express";
 import ratesController from "./rates.controller"
 import { validateSchema } from "../../middleware/validateSchema";
 import { rateSchema } from "./rates.schema";
-const { createRate, deleteRate } = ratesController;
+const { createRate, deleteRate, getRatesByRecipe, deleteRatesByRecipeID} = ratesController;
 
 const ratesRoutes = express.Router();
 
 ratesRoutes.post("/", validateSchema(rateSchema), createRate);
 ratesRoutes.delete("/:id", deleteRate);
+ratesRoutes.get("/:id", getRatesByRecipe);
+ratesRoutes.delete("/recipe/:id", deleteRatesByRecipeID);
 
 export default ratesRoutes;
 
