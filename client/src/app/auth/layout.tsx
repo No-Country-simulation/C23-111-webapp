cae7dd1164b402012e27a43e7a3a285d7681ecc1
BrunoDiaz7 <brunoleandrodiaz07@gmail.com>
2025-01-21 14:25:06 -0300

    refactor: cambio en rutas

---
+++ client/src/app/auth/layout.tsx
@@ -0,0 +1,10 @@
+export default function AuthLayout ({children}: {children: React.ReactNode}) {
+    return (
+        <html lang="es">
+            <title>Registrate</title>
+            <body>
+                {children}
+            </body>
+        </html>
+    )
+}
