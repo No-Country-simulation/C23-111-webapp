6490da3c42d5e888ae3f76880570ddc6d1be4340
BrunoDiaz7 <brunoleandrodiaz07@gmail.com>
2025-02-03 20:01:15 -0300

    feat: crear my-recipes page

---
+++ client/src/app/user/my-recipes/page.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const UserPage =()=> {
+  return (
+    <Typography variant="h1">
+      Mis Recetas
+    </Typography>
+  );
+}
+
+export default UserPage
