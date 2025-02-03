543aa9b0e92f40b3f3ed7ce7c5dad38571c3789b
BrunoDiaz7 <brunoleandrodiaz07@gmail.com>
2025-02-03 16:50:53 -0300

    style: formato

--- client/src/services/rates/index.ts
+++ client/src/services/rates/index.ts
@@ -3,29 +3,29 @@
 import { privateInstance, publicInstance } from "../axios";
 
 export const getRatesById = async (id: string) => {
-    try {
-        const response = await publicInstance.get(`/rates/${id}`)
-        return response;
-    } catch (error) {
-        throw error;
-    }
-}
-
-console.log(getRatesById);
+  try {
+    const response = await publicInstance.get(`/rates/${id}`);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
 
 type RateData = {
-    rating: number;
-    comment: string;
-    reviewer: string;
-    recipe: string;
-  
-}
+  rating: number;
+  comment: string;
+  reviewer: string;
+  recipe: string;
+};
 
-export const addRateById = async (id: string,data: RateData) => {
-    try {
-        const response = await privateInstance.post(`/rates/`,{...data,recipe:id})
-        return response;
-    } catch (error) {
-        throw error;
-    }
-}
+export const addRateById = async (id: string, data: RateData) => {
+  try {
+    const response = await privateInstance.post(`/rates/`, {
+      ...data,
+      recipe: id,
+    });
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
