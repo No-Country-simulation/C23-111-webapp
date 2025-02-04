cf4c2b4f5d4ce57b24386a5dcbeee764f88ca571
BrunoDiaz7 <brunoleandrodiaz07@gmail.com>
2025-02-04 20:12:16 -0300

    feat: crear pagina my-recipes

--- client/src/app/user/my-recipes/page.tsx
+++ client/src/app/user/my-recipes/page.tsx
@@ -1,20 +1,21 @@
 "use client";
-import {  Typography, styled } from "@mui/material";
+import { Box, Typography, styled } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useAuth } from "@/context/authContext";
 import { getRecipeByUserId } from "@/services/recipes";
 import { recipe } from "@/types/recipes";
-import { MainLoader, RecipeCard, Header } from "@/Components";
+import { MainLoader, RecipeCard, Header, AddRecipeModal } from "@/Components";
 import { toast } from "react-toastify";
+import Grid from "@mui/material/Grid2";
 
-const PageContainer = styled('main') ({
-  display: 'flex',
-  flexDirection: 'column',
-  width: '100%',
-  alignItems: 'center',
+const PageContainer = styled("main")({
+  display: "flex",
+  flexDirection: "column",
+  width: "100%",
+  alignItems: "center",
   padding: 0,
-  gap: '10px'
-})
+  gap: "10px",
+});
 
 const UserPage = () => {
   const { user } = useAuth();
@@ -28,11 +29,11 @@ const UserPage = () => {
 
     const fetchRecipes = async (id: string) => {
       try {
-        setLoading(true)
+        setLoading(true);
         const response = await getRecipeByUserId(id);
         setUserRecipes(response.data.user.myRecipes);
       } catch (error) {
-        toast.error(`Error al obtener recetas: ${error}`)
+        toast.error(`Error al obtener recetas: ${error}`);
       } finally {
         setLoading(false);
       }
@@ -43,27 +44,38 @@ const UserPage = () => {
 
   return (
     <PageContainer>
-      <Header sx={{width: '100%'}} />
-      <Typography sx={{marginTop: '2%'}} variant="h1">Mis Recetas</Typography>
-      <Typography variant="h2">Has subido <span style={{color: '#F48E28'}}>{userRecipes.length}</span> recetas</Typography>
+      <Header sx={{ width: "100%" }} />
+      <Typography sx={{ marginTop: "2%" }} variant="h1">
+        Mis Recetas
+      </Typography>
+      <Typography variant="h2">
+        Has subido{" "}
+        <span style={{ color: "#F48E28" }}>{userRecipes.length}</span> recetas
+      </Typography>
+      <Box className='flex flex-col items-center mt-10'>
+        <Typography variant="h4" sx={{fontWeight: 500}}>
+          ¿Tienes más creaciones para compartir?
+        </Typography>
+        <AddRecipeModal />
+      </Box>
       {loading ? (
         <MainLoader isOpen={loading} />
       ) : (
-        <div>
+        <Grid container spacing={2} className="flex justify-center mt-15">
           {userRecipes?.map((recipe) => (
-            <RecipeCard
-            key={recipe.id}
-            recipe={recipe}
-            _id={recipe._id}
-            status={recipe.status}
-            sx={{margin: '2% 0'}}
-            />
+            <Grid key={recipe.id} size={{ lg: 5, md: 7 }}>
+              <RecipeCard
+                recipe={recipe}
+                _id={recipe._id}
+                status={recipe.status}
+                sx={{ margin: "2% 0" }}
+              />
+            </Grid>
           ))}
-        </div>
+        </Grid>
       )}
     </PageContainer>
   );
 };
 
 export default UserPage;
-
